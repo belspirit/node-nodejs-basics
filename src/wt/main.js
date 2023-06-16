399450b589b58aba23666a94d69fc0a4ce48f451
Sergey <sergeylabetsik@datateer.com>
2023-06-16 17:39:45 -0500

    worker_threads part 2

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,25 +1,34 @@
 import { fileURLToPath } from "node:url";
 import path from "node:path";
 import { Worker } from "node:worker_threads";
+import os from "node:os";
 
 const performCalculations = async () => {
-  const n = 8;
+  let n = 10;
 
   const scriptPath = fileURLToPath(import.meta.url);
   const dir = path.dirname(scriptPath);
   const workerPath = path.join(dir, "worker.js");
-  const worker = new Worker(workerPath, { workerData: n });
-  worker.on("message", (result) => {
-    console.log(`nthFibonacci(${n}) = ${result}`);
-  });
-  worker.on("error", (err) => {
-    throw err;
-  });
-  worker.on("exit", (exitCode) => {
-    if (exitCode !== 0) {
-      throw new Error(`Worker stopped with exit code ${code}`);
-    }
-  });
+  const results = await Promise.all(
+    os.cpus().map(
+      () =>
+        new Promise((resolve, reject) => {
+          const worker = new Worker(workerPath, { workerData: n++ });
+          worker.on("message", (result) => {
+            resolve({ data: result, status: "resolved" });
+          });
+          worker.on("error", (err) => {
+            resolve({ data: null, status: "error" });
+          });
+          worker.on("exit", (exitCode) => {
+            if (exitCode !== 0) {
+              resolve({ data: null, status: "error" });
+            }
+          });
+        })
+    )
+  );
+  console.log(results);
 };
 
 await performCalculations();
