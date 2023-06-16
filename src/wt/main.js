ee336427951392128992c7507b82bacf3769c4cb
Sergey <sergeylabetsik@datateer.com>
2023-06-16 16:05:56 -0500

    worker_threads part 1

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { Worker } from "node:worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const n = 4;
+  // Write your code here
+  const scriptPath = fileURLToPath(import.meta.url);
+  const dir = path.dirname(scriptPath);
+  const workerPath = path.join(dir, "worker.js");
+  const worker = new Worker(workerPath, { workerData: n });
+  worker.on("message", (result) => {
+    console.log(`Result = ${result}`);
+  });
+  worker.on("error", (err) => {
+    throw err;
+  });
+  worker.on("exit", (exitCode) => {
+    if (exitCode !== 0) {
+      throw new Error(`Worker stopped with exit code ${code}`);
+    }
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import { parentPort, workerData } from "node:worker_threads";
+
+const n = workerData;
+
+if (n === undefined) throw new Error("n should be an integer number");
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(nthFibonacci(n));
 };
 
 sendResult();
